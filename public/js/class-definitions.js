6d8cec9029fd5f627033538a0ee0533451ed0807
mellaniesoriano <msoriano808@gmail.com>
2017-06-17 11:02:46 -1000

    browsers passed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -153,6 +153,15 @@
  *
  */
 
+var browsers = {
+  Chromium : "google.com",
+  Safari : "apple.com",
+  Opera : "opera.com",
+  Firefox : "mozilla.org",
+  Sleipnir : "fenrir-inc.com",
+  Konqueror : "konqueror.org"
+};
+
 
 /* Step 13
  *
