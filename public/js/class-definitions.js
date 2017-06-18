de1be91e87efed27a8d869adf315e869d75b4aa0
mellaniesoriano <msoriano808@gmail.com>
2017-06-17 19:18:26 -1000

    99 done

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1526,8 +1526,8 @@ var porchLightsOff = new Lightbulb(false).flipSwitch();
  * and assign the values to each variable below.
  *
  */
-var cookieMonsterPwns;
-var cookieMonsterBlocked;
+var cookieMonsterPwns = new Cookie('chocolate').swipedByCookieMonster('Monday');
+var cookieMonsterBlocked = new Cookie('peanut butter').swipedByCookieMonster('Saturday');
 
 
  /* Step 100
